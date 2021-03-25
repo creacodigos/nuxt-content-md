2cc3787176ebe862eb6c86de8bc2c394e0214c4b
Jorge Santana <jorge@creacodigos.com>
2021-03-25 15:26:30 +0000

    PRUEBAS - Borrados docs/

--- docs/_nuxt/static/1616351061/blog/author/Jorge Santana/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-content-md\u002F_nuxt\u002Fstatic\u002F1616351061",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fauthor\u002FJorge%20Santana",config:{app:{basePath:"\u002Fnuxt-content-md\u002F",assetsPath:"\u002Fnuxt-content-md\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e45107c7"}}}}(null));
